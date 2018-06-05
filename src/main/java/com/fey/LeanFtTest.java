e8dba868709f896e1d761e022fad855bb4ee73e9
MikeFey <michael.j.fey@microfocus.com>
2018-06-05 02:05:26 +0000

    user story #3001: implement

--- src/main/java/com/fey/LeanFtTest.java
+++ src/main/java/com/fey/LeanFtTest.java
@@ -51,7 +51,7 @@ public class LeanFtTest extends UnitTestClassBase {
 
         bd.set("osVersion", "10");
 
-        bd.set("testName", "Fey LRT SRF  Remote Execution demo");
+        bd.set("testName", "Fey LRT SRF  Remote Execution demo2");
 
         Browser browser = SrfLab.launchBrowser(bd);
 
